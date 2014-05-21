3de64ddebeb45926bb2341219dcf6f7793a75ec6
Ben Thompson <t.ben.thompson@gmail.com>
2014-05-21 11:06:31 -0400

    Recursive legendre for the inner integral works.

--- recursive_legendre.py
+++ recursive_legendre.py
@@ -1,4 +1,10 @@
+import numpy as np
 from math import sqrt, log
+
+"""
+This is all pretty sloppy. Clean it up before implementing in any real
+application!
+"""
 # The starting value definitions are taken from the Aimi and Diligenti 2002
 # paper
 def k1(ay, by):
@@ -26,4 +32,71 @@ def mu_1_3(ay, by):
     term2 = -(20 * by ** 2 - ((55 * ay + 25) * ay - 8)) * k1(ay, by) / 12
     term3 = +(20 * by ** 2 - ((55 * ay - 25) * ay - 8)) * k2(ay, by) / 12
     return term1 + term2 + term3
+
+# The recursion for the legendre polynomials
+k = np.arange(40, dtype=np.float64)
+alpha = (2 * k + 1) / (k + 1)
+beta = k / (k + 1)
+
+def P0(x):
+    return 1
+
+def P1(x):
+    return x
+
+def P(n, x):
+    """ Only call for n > 2 """
+    pass
+
+
+def A(k, m, ay, by):
+    return ((2 * k - 1) / (m - 2)) * (2 * ay / alpha[k - 1]) - ay
+
+def B(k, m, ay, by):
+    term1 = (beta[k] * alpha[k - 2] - alpha[k]) / (alpha[k] * alpha[k - 2])
+    term2 = -(2 * k - 1) / (m - 2) * \
+            (((beta[k] * alpha[k - 2] + beta[k - 1] * alpha[k]) /\
+                    (alpha[k - 2] * alpha[k - 1] * alpha[k])) + \
+                (ay ** 2 + by ** 2))
+    return term1 + term2
+
+def C(k, m, ay, by):
+    return ay + (2 * k - 1) / (m - 2) * (2 * ay / alpha[k - 1] * beta[k - 1])
+
+def D(k, m, ay, by):
+    return - ((2 * k - 1) / (m - 2) *\
+            ((beta[k - 1] * beta[k - 2]) / (alpha[k - 2] * alpha[k - 1]))) - \
+            (beta[k - 2] / alpha[k - 2])
+
+def E(k, m, ay, by):
+    numer = (m - 2) * alpha[k - 1] * alpha[k]
+    denom = (2 * k - 1) - (m - 2) * alpha[k - 1]
+    return numer / denom
+
+def mu_1_kp1(k, m, ay, by, mu_1_k, mu_1_km1, mu_1_km2, mu_1_km3):
+    Ak = A(k, m, ay, by)
+    Bk = B(k, m, ay, by)
+    Ck = C(k, m, ay, by)
+    Dk = D(k, m, ay, by)
+    Ek = E(k, m, ay, by)
+    Ak *= Ek
+    Bk *= Ek
+    Ck *= Ek
+    Dk *= Ek
+    next = Dk * mu_1_km3 + Ck * mu_1_km2 +\
+           Bk * mu_1_km1 + Ak * mu_1_k
+    return next
+
+def mu_1(k, m, ay, by):
+    mu_1_km3 = mu_1_0(ay, by)
+    mu_1_km2 = mu_1_1(ay, by)
+    mu_1_km1 = mu_1_2(ay, by)
+    mu_1_k = mu_1_3(ay, by)
+    for cur_k in range(3, k):
+        next_mu = mu_1_kp1(cur_k, 1, ay, by, mu_1_k, mu_1_km1, mu_1_km2, mu_1_km3)
+        mu_1_km3 = mu_1_km2
+        mu_1_km2 = mu_1_km1
+        mu_1_km1 = mu_1_k
+        mu_1_k = next_mu
+    return mu_1_k
 
--- test_recursive_legendre.py
+++ test_recursive_legendre.py
@@ -1,9 +1,21 @@
 import numpy as np
 from recursive_legendre import *
 
+def test_alpha():
+    assert(alpha[0] == 1.0 / 1.0)
+    assert(alpha[1] == 3.0 / 2.0)
+    assert(alpha[2] == 5.0 / 3.0)
+    assert(alpha[3] == 7.0 / 4.0)
+
+# Exact integrals computed using mathematica
 def test_init0():
     est = mu_1_0(1.2, 1.2)
     exact = 1.20061
+    np.testing.assert_almost_equal(est, exact, 5)
+
+def test_init0_singular():
+    est = mu_1_0(0.8, 0.8)
+    exact = 1.79762
     np.testing.assert_almost_equal(est, exact, 5)
 
 def test_init1():
@@ -11,12 +23,45 @@ def test_init1():
     exact = 0.151292
     np.testing.assert_almost_equal(est, exact, 5)
 
+def test_init1_singular():
+    est = mu_1_1(0.6, 0.6)
+    exact = 0.411843
+    np.testing.assert_almost_equal(est, exact, 5)
+
 def test_init2():
     est = mu_1_2(1.2, 1.2)
     exact = 0.00677433
+    np.testing.assert_almost_equal(est, exact, 5)
+
+def test_init2_singular():
+    est = mu_1_2(0.01, 0.01)
+    exact = -3.79787
     np.testing.assert_almost_equal(est, exact, 5)
 
 def test_init3():
     est = mu_1_3(1.2, 1.2)
     exact = -0.00407733
     np.testing.assert_almost_equal(est, exact, 5)
+
+def test_init3_singular():
+    est = mu_1_3(-0.01, -0.01)
+    exact = 0.103966
+    np.testing.assert_almost_equal(est, exact, 5)
+
+def test_value():
+    ay = 0.05
+    by = 0.05
+    pairs = [(4,1.1977), (10,-0.376323),
+             (20, 0.0743193), (30, -0.00279826),
+             (31, -0.05510287928339709)]
+    for n, exact in pairs:
+        est = mu_1(n, 1, ay, by)
+        np.testing.assert_almost_equal(est, exact, 5)
+
+def test_distant_value():
+    ay = 5.0
+    by = 5.0
+    exact = 9.68812e-8
+    est = mu_1(9, 1, ay, by)
+    np.testing.assert_almost_equal(est, exact, 5)
+    print est - exact
